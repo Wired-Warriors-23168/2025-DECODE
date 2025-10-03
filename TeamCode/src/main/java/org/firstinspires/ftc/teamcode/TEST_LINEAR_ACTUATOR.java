73e161a7a5b66b2f1a8d4b18ea0bf12a11fd8992
Wired-Warriors <FTC.23168@gmail.com>
2025-10-03 15:43:40 -0400

    Update TEST_LINEAR_ACTUATOR.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST_LINEAR_ACTUATOR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST_LINEAR_ACTUATOR.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -14,6 +17,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class TEST_LINEAR_ACTUATOR extends LinearOpMode {
 //TODO ************* THIS IS THE TELEOP FROM THE 6TH GRADE BOT, UPDATE IT FOR THE 23168 BOT!!!!
     public Servo myServo;
+    public double setPos;
 
     // Setting our velocity targets. These values are in ticks per second!
 
@@ -27,10 +31,12 @@ public class TEST_LINEAR_ACTUATOR extends LinearOpMode {
         if (myServo instanceof PwmControl) {
             PwmControl pwmControl = (PwmControl) myServo;
             // Set a custom PWM range (optional, default is usually fine)
-            PwmControl.PwmRange range = new PwmControl.PwmRange(500, 2500);
+            PwmControl.PwmRange range = new PwmControl.PwmRange(1000, 2000);
             pwmControl.setPwmRange(range);
             // Enable PWM output
             pwmControl.setPwmEnable();
+            setPos=0.5;
+            myServo.setPosition(setPos);
         }
 
         waitForStart();
@@ -38,15 +44,22 @@ public class TEST_LINEAR_ACTUATOR extends LinearOpMode {
             while (opModeIsActive()) {
                 // Calling our methods while the OpMode is running
                 manualLinearActuator();
+                telemetry.addData("Set Position", setPos);
+                telemetry.update();
 
             }
         }
     }private void manualLinearActuator() {
         // Manual control for the linear actuator
+
         if (gamepad1.dpad_left) {
-            myServo.setPosition(0.2);
+            setPos=0;
+            setPos=max(setPos,0);
+            myServo.setPosition(setPos);
         } else if (gamepad1.dpad_right) {
-            myServo.setPosition(0.8);
+            setPos=1;
+            setPos=min(setPos,1.0);
+            myServo.setPosition(setPos);
         }
     }
 
