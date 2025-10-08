90f505e55e404b37b694a2fad7a59b9bc50d3357
CamGuyMan <Ruggy05@gmail.com>
2025-10-07 20:25:24 -0400

    will set motor to specific position when it is endgame and button is pressed. exact position is not specified (currently 0)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP_LIFT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP_LIFT.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 public class TELEOP_LIFT extends LinearOpMode {
@@ -13,6 +14,9 @@ public class TELEOP_LIFT extends LinearOpMode {
     // Declare OpMode members.
     private DcMotor lift;
 
+    private ElapsedTime runtime = new ElapsedTime();
+    final int extensionposition = 0;
+    final int packagedposition = 0;
     /////////////////////////////////////////////////////////////////////////
     // Declare variables
 
@@ -23,32 +27,44 @@ public class TELEOP_LIFT extends LinearOpMode {
         lift = hardwareMap.get(DcMotor.class, "motor-lift");
 
         // Establishing the direction and mode for the motors
+        lift.setTargetPosition(0);
+        lift.setPower(0.5);
         lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         lift.setDirection(DcMotor.Direction.REVERSE);
 
-
         waitForStart();
+        runtime.reset();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                // Calling our methods while the OpMode is running
-                runLift();
-                //telemetry.addData("Lift Position", ();
-                telemetry.update();
+                lift();
             }
         }
     }
 
 
     /**
-     * The runLift function is designed to run the lift to lift the robot in endgame.
-     * When running this function, the lift motor will run to a defined position and then hold there, even if the driver releases the button.
-     * This function will be activated by pressing two buttons at the same time for safety.
+     * The lift function is designed to run the lift to lift the robot in endgame.
+     * When running this function, the lift motor will run to a defined position and adjust for ally height with manual player input.
+     * This function will be eventually activated only after a timer for 180 seconds goes off and a button is pressed for safety.
      */
-    private void runLift() {
 
+
+private void lift() {
+    if (runtime.time() > 100) {
+        if (gamepad1.start) {
+            lift.setTargetPosition(extensionposition);
+        } else if (gamepad1.back) {
+            lift.setTargetPosition(packagedposition);
+        }
     }
-
-
+    //telemetry.addData("Lift Position", ();
+    telemetry.addData("controller start", gamepad1.start);
+    telemetry.addData("controller back", gamepad1.back);
+    telemetry.addData("expected lift position", lift.getTargetPosition());
+    telemetry.addData("lift position", lift.getCurrentPosition());
+    telemetry.addData("time", runtime.time());
+    telemetry.update();
+}
 
 
 
