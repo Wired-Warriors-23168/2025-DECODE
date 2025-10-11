11aac3f51ecaeede79be12727c4b5fb4b7e00518
a-krier <kriergenics@gmail.com>
2025-10-11 15:27:27 -0400

    Added OTOS Sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REVStarterBotTeleOpJava.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REVStarterBotTeleOpJava.java
@@ -10,6 +10,9 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
 
@@ -25,6 +28,7 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
     private DcMotor rightFrontDrive;
     private DcMotor rightBackDrive;
     private RevBlinkinLedDriver lightsLED;
+    SparkFunOTOS poseOTOS;
 
     // Declare variables
     // Set as "static" and not "final" in order to be able to tune parameters in FTCDashboard
@@ -43,6 +47,7 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
         rightBackDrive = hardwareMap.get(DcMotor.class, "right-back-drive");
         agitator = hardwareMap.get(CRServo.class, "servo-agitator");
         lightsLED = hardwareMap.get(RevBlinkinLedDriver.class,"pwm-LED");
+        poseOTOS = hardwareMap.get(SparkFunOTOS.class, "sensor-otos");
 
         // Establishing the direction and mode for the motors
         // flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -56,9 +61,16 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
         //Ensures the agitator is active and ready
         agitator.setPower(0);
 
+        // All the configuration for the OTOS is done in this helper method, check it out!
+        configureOtos();
+
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
+                // Get the latest position, which includes the x and y coordinates, plus the
+                // heading angle
+                SparkFunOTOS.Pose2D pos = poseOTOS.getPosition();
+
                 // Calling our methods while the OpMode is running
                 splitStickArcadeDrive();
                 setFlywheelVelocity();
@@ -68,6 +80,11 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
                 //Set up the telemetry to the driver hub
                 //telemetry.addData("Flywheel Velocity", ((DcMotorEx) flywheel).getVelocity());
                 telemetry.addData("Flywheel Power", flywheel.getPower());
+                // Log the position to the telemetry
+                telemetry.addData("X coordinate", pos.x);
+                telemetry.addData("Y coordinate", pos.y);
+                telemetry.addData("Heading angle", pos.h);
+
                 telemetry.update();
 
                 /////////////////////////////////////////////////////////////////////////////////
@@ -192,5 +209,87 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
             feeder.setPower(0);
         }
     }*/
+
+    private void configureOtos() {
+        telemetry.addLine("Configuring OTOS...");
+        telemetry.update();
+
+        // Set the desired units for linear and angular measurements. Can be either
+        // meters or inches for linear, and radians or degrees for angular. If not
+        // set, the default is inches and degrees. Note that this setting is not
+        // persisted in the sensor, so you need to set at the start of all your
+        // OpModes if using the non-default value.
+        // poseOTOS.setLinearUnit(DistanceUnit.METER);
+        poseOTOS.setLinearUnit(DistanceUnit.INCH);
+        // poseOTOS.setAngularUnit(AnguleUnit.RADIANS);
+        poseOTOS.setAngularUnit(AngleUnit.DEGREES);
+
+        // Assuming you've mounted your sensor to a robot and it's not centered,
+        // you can specify the offset for the sensor relative to the center of the
+        // robot. The units default to inches and degrees, but if you want to use
+        // different units, specify them before setting the offset! Note that as of
+        // firmware version 1.0, these values will be lost after a power cycle, so
+        // you will need to set them each time you power up the sensor. For example, if
+        // the sensor is mounted 5 inches to the left (negative X) and 10 inches
+        // forward (positive Y) of the center of the robot, and mounted 90 degrees
+        // clockwise (negative rotation) from the robot's orientation, the offset
+        // would be {-5, 10, -90}. These can be any value, even the angle can be
+        // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
+        SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
+        poseOTOS.setOffset(offset);
+
+        // Here we can set the linear and angular scalars, which can compensate for
+        // scaling issues with the sensor measurements. Note that as of firmware
+        // version 1.0, these values will be lost after a power cycle, so you will
+        // need to set them each time you power up the sensor. They can be any value
+        // from 0.872 to 1.127 in increments of 0.001 (0.1%). It is recommended to
+        // first set both scalars to 1.0, then calibrate the angular scalar, then
+        // the linear scalar. To calibrate the angular scalar, spin the robot by
+        // multiple rotations (eg. 10) to get a precise error, then set the scalar
+        // to the inverse of the error. Remember that the angle wraps from -180 to
+        // 180 degrees, so for example, if after 10 rotations counterclockwise
+        // (positive rotation), the sensor reports -15 degrees, the required scalar
+        // would be 3600/3585 = 1.004. To calibrate the linear scalar, move the
+        // robot a known distance and measure the error; do this multiple times at
+        // multiple speeds to get an average, then set the linear scalar to the
+        // inverse of the error. For example, if you move the robot 100 inches and
+        // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
+        poseOTOS.setLinearScalar(1.0);
+        poseOTOS.setAngularScalar(1.0);
+
+        // The IMU on the OTOS includes a gyroscope and accelerometer, which could
+        // have an offset. Note that as of firmware version 1.0, the calibration
+        // will be lost after a power cycle; the OTOS performs a quick calibration
+        // when it powers up, but it is recommended to perform a more thorough
+        // calibration at the start of all your OpModes. Note that the sensor must
+        // be completely stationary and flat during calibration! When calling
+        // calibrateImu(), you can specify the number of samples to take and whether
+        // to wait until the calibration is complete. If no parameters are provided,
+        // it will take 255 samples and wait until done; each sample takes about
+        // 2.4ms, so about 612ms total
+        poseOTOS.calibrateImu();
+
+        // Reset the tracking algorithm - this resets the position to the origin,
+        // but can also be used to recover from some rare tracking errors
+        poseOTOS.resetTracking();
+
+        // After resetting the tracking, the OTOS will report that the robot is at
+        // the origin. If your robot does not start at the origin, or you have
+        // another source of location information (eg. vision odometry), you can set
+        // the OTOS location to match and it will continue to track from there.
+        SparkFunOTOS.Pose2D currentPosition = new SparkFunOTOS.Pose2D(0, 0, 0);
+        poseOTOS.setPosition(currentPosition);
+
+        // Get the hardware and firmware version
+        SparkFunOTOS.Version hwVersion = new SparkFunOTOS.Version();
+        SparkFunOTOS.Version fwVersion = new SparkFunOTOS.Version();
+        poseOTOS.getVersionInfo(hwVersion, fwVersion);
+
+        //telemetry.addLine("OTOS configured! Press start to get position data!");
+        //telemetry.addLine();
+        telemetry.addLine(String.format("OTOS Hardware Version: v%d.%d", hwVersion.major, hwVersion.minor));
+        telemetry.addLine(String.format("OTOS Firmware Version: v%d.%d", fwVersion.major, fwVersion.minor));
+        telemetry.update();
+    }
 
 }
