e74a3d750d1de076a1ce4bf17ba5d9e35775da09
a-krier <kriergenics@gmail.com>
2025-10-16 18:15:23 -0400

    OTOS position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSLocalizer.java
@@ -19,7 +19,7 @@ public class OTOSLocalizer implements Localizer {
         public double linearScalar = 1.0;
 
         // Note: units are in inches and radians
-        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
+        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0.65625, 0);
     }
 
     public static Params PARAMS = new Params();
