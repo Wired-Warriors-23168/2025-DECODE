dc483d5d29021052011e6eb214953a70a474a549
jumarkus712 <jumarkus712@gmail.com>
2025-10-21 19:58:56 -0400

    merged in intake to teleop_main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP_MAIN.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP_MAIN.java
@@ -2,11 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -15,6 +19,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @Config //Required to be able to tune parameters in FTCDashboard
 @TeleOp
 public class TELEOP_MAIN extends LinearOpMode {
+    private DcMotor intake;
+    private Servo selector;
+    private ColorSensor colorSensorA;
+    private ColorSensor colorSensorB;
+    private CRServo conveyorG;
+    private CRServo conveyorP;
     private DcMotor leftFrontDrive;
     private DcMotor leftBackDrive;
     private DcMotor rightFrontDrive;
@@ -37,21 +47,36 @@ public class TELEOP_MAIN extends LinearOpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
 
+        intake = hardwareMap.get(DcMotor.class, "motor-intake");
+        selector = hardwareMap.get(Servo.class, "servo-selector");
+        colorSensorA = hardwareMap.get(ColorSensor.class, "sensor-color-A");
+        colorSensorB = hardwareMap.get(ColorSensor.class, "sensor-color-B");
+        conveyorG = hardwareMap.get(CRServo.class, "green-conveyor");
+        conveyorP = hardwareMap.get(CRServo.class, "purple-conveyor");
+
+
+        // Establishing the direction and mode for the motors
+        intake.setDirection(DcMotor.Direction.REVERSE);
+        selector.setDirection(Servo.Direction.FORWARD);
+        conveyorG.setDirection(CRServo.Direction.FORWARD);
+        conveyorP.setDirection(CRServo.Direction.FORWARD);
+        selector.setDirection(Servo.Direction.FORWARD);
+
+
         configureOtos();
 
         waitForStart();
         if (opModeIsActive()) {
+            conveyorG.setPower(1);
+            conveyorP.setPower(1);
             while (opModeIsActive()) {
 
-                // Calling our methods while the OpMode is running
+                intakeSort(false);
                 drivetrain();
 
                 FtcDashboard dashboard = FtcDashboard.getInstance();
                 TelemetryPacket packet = new TelemetryPacket();
-
                 dashboard.sendTelemetryPacket(packet); // Always send the packet
-
-                //Set up the Field overlay
                 packet.fieldOverlay()
                         .setFill("blue")
                         .fillRect(-20, -20, 40, 40);
@@ -62,6 +87,46 @@ public class TELEOP_MAIN extends LinearOpMode {
         }
     }
 
+    public void intakeSort(boolean auto) {
+
+        sortArtifact();
+
+        if(gamepad2.right_bumper){
+            intake.setPower(-1);
+            selector.setPosition(0.75);
+        }
+        if(gamepad2.right_trigger > 0.2 || auto){
+            intake.setPower(1);
+        } else {
+            intake.setPower(0);
+        }
+    }
+
+    public void sortArtifact() {
+        float sumGreenPurpleness = greenPurplenessA() + greenPurplenessB();
+        telemetry.addData("sumGreenPurpleness",sumGreenPurpleness);
+        if(sumGreenPurpleness > 120){
+            selector.setPosition(1);
+        }
+        else if (sumGreenPurpleness < -70 ) {
+            selector.setPosition(0.5);
+        }
+        else {
+            selector.setPosition(0.75);
+        }
+        telemetry.addData("selector_pos",selector.getPosition());
+    }
+
+    public float greenPurplenessA(){
+        int  greenA = colorSensorA.green();
+        int purpleA = (colorSensorA.red() + colorSensorA.blue())/2;
+        return greenA - purpleA;
+    }
+    private float greenPurplenessB(){
+        int  greenB = colorSensorB.green();
+        int purpleB = (colorSensorB.red() + colorSensorB.blue())/2;
+        return greenB - purpleB;
+    }
     private void drivetrain() {
         double x;
         double y;
@@ -92,6 +157,7 @@ public class TELEOP_MAIN extends LinearOpMode {
         telemetry.addData("Heading angle", pos.h);
     }
 
+
     private void configureOtos() {
         telemetry.addLine("Configuring OTOS...");
         telemetry.update();
@@ -105,6 +171,7 @@ public class TELEOP_MAIN extends LinearOpMode {
         poseOTOS.setLinearUnit(DistanceUnit.INCH);
         // poseOTOS.setAngularUnit(AnguleUnit.RADIANS);
         poseOTOS.setAngularUnit(AngleUnit.DEGREES);
+
 
         // Assuming you've mounted your sensor to a robot and it's not centered,
         // you can specify the offset for the sensor relative to the center of the
