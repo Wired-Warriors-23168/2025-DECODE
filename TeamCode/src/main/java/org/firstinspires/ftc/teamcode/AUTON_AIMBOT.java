2f89d205531a04f466ac742d8b143bee3bfa7c1d
jumarkus712 <jumarkus712@gmail.com>
2025-10-21 20:40:28 -0400

    jayden's shooter code (Levi)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTON_AIMBOT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTON_AIMBOT.java
@@ -9,37 +9,25 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import java.util.List;
 
 @Autonomous
 public class AUTON_AIMBOT extends LinearOpMode {
-//TODO ************* THIS IS BASED ON THE TELEOP FROM THE 6TH GRADE BOT, UPDATE IT FOR THE 23168 BOT!!!!
 
     // Declare OpMode members.
     private DcMotor flywheel;
     private Limelight3A limelight;
 
-    private CRServo purpleServo;
-    private CRServo greenServo;
-
-    private DcMotor leftFrontDrive;
-    private DcMotor leftBackDrive;
-    private DcMotor rightFrontDrive;
-    private DcMotor rightBackDrive;
-
-
-    private double shootposition = 10;
+    private Servo purpleServo;
+    private Servo greenServo;
 
     private boolean seenobelisk = false;
 
     private int teamPipeline = 0; // blue auton
     private int patternID = 0;
 
-    private long shootTimeMS = 1500;
-
-    /////////////////////////////////////////////////////////////////////////
-    // Declare variables
     public double txLimelight;
     public double tyLimelight;
     double tx = 0;
@@ -48,25 +36,18 @@ public class AUTON_AIMBOT extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-       // flywheel = hardwareMap.get(DcMotor.class, "motor-flywheel");
+        flywheel = hardwareMap.get(DcMotor.class, "motor-flywheel");
         limelight = hardwareMap.get(Limelight3A.class,"limelight");
-        greenServo = hardwareMap.get(CRServo.class, "greenServo");
-        purpleServo = hardwareMap.get(CRServo.class, "purpleServo");
-        // Establishing the direction and mode for the motors
-      //  flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      //  flywheel.setDirection(DcMotor.Direction.REVERSE);
-        purpleServo.setDirection(DcMotorSimple.Direction.FORWARD);
-        greenServo.setDirection(DcMotorSimple.Direction.FORWARD);
+        greenServo = hardwareMap.get(Servo.class, "greenServo");
+        purpleServo = hardwareMap.get(Servo.class, "purpleServo");
 
-        leftFrontDrive = hardwareMap.get(DcMotor.class, "left-front-drive");
-//        leftBackDrive = hardwareMap.get(DcMotor.class, "left-back-drive");
-//        rightFrontDrive = hardwareMap.get(DcMotor.class, "right-front-drive");
-//        rightBackDrive = hardwareMap.get(DcMotor.class, "right-back-drive");
+        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheel.setDirection(DcMotor.Direction.REVERSE);
+        greenServo.setDirection(Servo.Direction.FORWARD);
+        purpleServo.setDirection(Servo.Direction.FORWARD);
 
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-//        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-//        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-//        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        greenServo.setPosition(0);
+        purpleServo.setPosition(0);
 
         telemetry.setMsTransmissionInterval(11);
 
@@ -107,7 +88,7 @@ public class AUTON_AIMBOT extends LinearOpMode {
             tagID = 0;
             limelight.pipelineSwitch(teamPipeline);
         }
-        if (gamepad2.left_trigger > 0.5 && result.isValid()) {
+        if (gamepad2.left_trigger > 0.5 ) {  //&& result.isValid()+
             if (patternID == 22 && tagID == 20) {
                 rotate();
                 if (Math.abs(tx) < 5) {
@@ -124,57 +105,63 @@ public class AUTON_AIMBOT extends LinearOpMode {
                     Pattern23(); // Purple purple green
                 }
             }
-        }
-
-        if (gamepad1.dpad_right) {             // Forced shooting: Purple
-            purpleServo.setPower(1);
-            sleep(shootTimeMS);
-            purpleServo.setPower(0);
-        } else if (gamepad1.dpad_left) {      // Forced shooting: Green
-            greenServo.setPower(1);
-            sleep(shootTimeMS);
-            greenServo.setPower(0);
+        } else if (gamepad2.dpadRightWasPressed()) {             // Forced shooting: Purple
+            purpleServo.setPosition(0.2);
+            sleep(250);
+            purpleServo.setPosition(0);
+        } else if (gamepad2.dpadLeftWasPressed()) {      // Forced shooting: Green
+            greenServo.setPosition(0.2);
+            sleep(250);
+            greenServo.setPosition(0);
         }
 
         telemetry.addData("Pattern ID", patternID);
         telemetry.addData("Seen obelisk", seenobelisk);
         telemetry.addData("Tag ID", tagID);
-      //  telemetry.addData("Flywheel Velocity", ((DcMotorEx) flywheel).getVelocity());
-      //  telemetry.addData("Flywheel Power", flywheel.getPower());
+        telemetry.addData("Flywheel Velocity", ((DcMotorEx) flywheel).getVelocity());
+        telemetry.addData("Flywheel Power", flywheel.getPower());
         telemetry.addData("Target X", tx);
         telemetry.addData("Target Y", ty);
     }
     private void Pattern22() {
-        purpleServo.setPower(1);
-        sleep(shootTimeMS);
-        purpleServo.setPower(0);
-        greenServo.setPower(1);
-        sleep(shootTimeMS);
-        greenServo.setPower(0);
-        purpleServo.setPower(1);
-        sleep(shootTimeMS);
-        purpleServo.setPower(0);
+        purpleServo.setPosition(0.2);
+        sleep(250);
+        purpleServo.setPosition(0);
+        greenServo.setPosition(0.2);
+        sleep(250);
+        greenServo.setPosition(0);
+        purpleServo.setPosition(0.2);
+        sleep(250);
+        purpleServo.setPosition(0);
     }
     private void Pattern21() {
-        greenServo.setPower(1);
-        sleep(shootTimeMS);
-        greenServo.setPower(0);
-        purpleServo.setPower(1);
-        sleep(2 * shootTimeMS);
-        purpleServo.setPower(0);
+        greenServo.setPosition(0.2);
+        sleep(250);
+        greenServo.setPosition(0);
+        purpleServo.setPosition(0.2);
+        sleep(250);
+        purpleServo.setPosition(0);
+        sleep(500);
+        purpleServo.setPosition(0.2);
+        sleep(250);
+        purpleServo.setPosition(0);
     }
     private void Pattern23() {
-        purpleServo.setPower(1);
-        sleep(2 * shootTimeMS);
-        purpleServo.setPower(0);
-        greenServo.setPower(1);
-        sleep(shootTimeMS);
-        greenServo.setPower(0);
+        purpleServo.setPosition(0.2);
+        sleep(250);
+        purpleServo.setPosition(0);
+        sleep(250);
+        purpleServo.setPosition(0.2);
+        sleep(250);
+        purpleServo.setPosition(0);
+        greenServo.setPosition(0.2);
+        sleep(250);
+        greenServo.setPosition(0);
     }
     private void rotate() {
         // spin drive with p controller
         double wheelpower = (tx/Math.abs(tx)) * 0.5; // TODO make p controller
-        leftFrontDrive.setPower(wheelpower);
+        //leftFrontDrive.setPower(wheelpower);
     }
 
 }
