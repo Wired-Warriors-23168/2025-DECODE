cb2884333efb6b8a670edec212a2e18dfb4f4c3c
a-krier <kriergenics@gmail.com>
2025-11-13 16:15:48 -0500

    first ATAN2 try

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP_MAIN_ANDY_TEST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP_MAIN_ANDY_TEST.java
@@ -1,35 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import static java.lang.Math.abs;
+import static java.lang.Math.atan2;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
-@Disabled
 @Config //Required to be able to tune parameters in FTCDashboard
 @TeleOp
-public class REVStarterBotTeleOpJava extends LinearOpMode {
+public class TELEOP_MAIN_ANDY_TEST extends LinearOpMode {
 
-    private DcMotorSimple flywheel;
-    private DcMotorSimple feeder;
+    private DcMotor flywheel;
+    private DcMotor feeder;
     private DcMotor leftFrontDrive;
     private DcMotor leftBackDrive;
-    private CRServo agitator;
     private DcMotor rightFrontDrive;
     private DcMotor rightBackDrive;
-    private RevBlinkinLedDriver lightsLED; //NOT the gobilda light...
 
     private Servo teamLED;
     SparkFunOTOS poseOTOS;
@@ -37,65 +34,74 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
     // Declare variables
     // Set as "static" and not "final" in order to be able to tune parameters in FTCDashboard
     // Setting our velocity targets. These values are in ticks per second!
-    private static double bankVelocity = 0.6;
-    private static double farVelocity = 0.85;
-    private static double maxVelocity = 0.5;
+    private static double bankVelocity = 1900;
+    private static double farVelocity = 2200;
+    private static double maxVelocity = 1300;
+    private double targetVelocity;
 
     public static final String ALLIANCE_KEY = "Alliance";
     public Object colorAlliance = blackboard.get(ALLIANCE_KEY);
 
+    public double blueGoalY = -71;
+    public double redGoalY = 71;
+    public double goalY;
+    public double goalX = -71;
+    public double goalHeading = 180;
+
+
     @Override
     public void runOpMode() {
-        flywheel = hardwareMap.get(DcMotorSimple.class, "motor-flywheel");
-        feeder = hardwareMap.get(DcMotorSimple.class, "motor-feeder");
+        flywheel = hardwareMap.get(DcMotor.class, "motor-flywheel");
+        feeder = hardwareMap.get(DcMotor.class, "motor-feeder");
         leftFrontDrive = hardwareMap.get(DcMotor.class, "left-front-drive");
         leftBackDrive = hardwareMap.get(DcMotor.class, "left-back-drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right-front-drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right-back-drive");
-        agitator = hardwareMap.get(CRServo.class, "servo-agitator");
-        lightsLED = hardwareMap.get(RevBlinkinLedDriver.class,"pwm-LED");
         poseOTOS = hardwareMap.get(SparkFunOTOS.class, "sensor-otos");
         teamLED = hardwareMap.get(Servo.class, "led-light");
 
         // Establishing the direction and mode for the motors
-        // flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        flywheel.setDirection(DcMotorSimple.Direction.REVERSE);
-        feeder.setDirection(DcMotor.Direction.REVERSE);
+        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheel.setDirection(DcMotor.Direction.REVERSE);
+        feeder.setDirection(DcMotor.Direction.FORWARD);
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        //Ensures the agitator is active and ready
-        agitator.setPower(0);
 
         // All the configuration for the OTOS is done in this helper method, check it out!
         configureOtos();
 
         waitForStart();
         if (opModeIsActive()) {
+
             while (opModeIsActive()) {
 
                 // Get the latest position, which includes the x and y coordinates, plus the
                 // heading angle
                 SparkFunOTOS.Pose2D pos = poseOTOS.getPosition();
 
+                //Set alliance-specific settings
                 if(colorAlliance=="BLUE"){
                     teamLED.setPosition(0.600); //blue
+                    goalY = blueGoalY;
                 }
                 else{
                     teamLED.setPosition(0.283);//red
+                    goalY = redGoalY;
                 }
 
                 // Calling our methods while the OpMode is running
                 splitStickArcadeDrive();
                 setFlywheelVelocity();
-                manualFeederAndagitatorControl();
+                manualFeederControl();
+                goalHeading();
 
                 /////////////////////////////////////////////////////////////////////////////////
                 //Set up the telemetry to the driver hub
                 telemetry.addData("Alliance", blackboard.get(ALLIANCE_KEY));
-                //telemetry.addData("Flywheel Velocity", ((DcMotorEx) flywheel).getVelocity());
+                telemetry.addData("Flywheel Velocity", ((DcMotorEx) flywheel).getVelocity());
                 telemetry.addData("Flywheel Power", flywheel.getPower());
                 // Log the position to the telemetry
                 telemetry.addData("X coordinate", pos.x);
@@ -104,21 +110,12 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
 
                 telemetry.update();
 
-                /////////////////////////////////////////////////////////////////////////////////
-                // Set up channels for display in FTCDashboard
-                FtcDashboard dashboard = FtcDashboard.getInstance();
-                TelemetryPacket packet = new TelemetryPacket();
-
-                // Send a value to the dashboard for graphing
-                dashboard.sendTelemetryPacket(packet); // Always send the packet
-                packet.put("Flywheel Power", flywheel.getPower()); // Robot-specific data
-                packet.put("Feeder Power", feeder.getPower()); // Robot-specific data
-                packet.put("Agitator Power", agitator.getPower()); // Robot-specific data
-
-                //Set up the Field overlay
-                packet.fieldOverlay()
-                        .setFill("blue")
-                        .fillRect(-20, -20, 40, 40);
+                if (Math.abs(((DcMotorEx) flywheel).getVelocity() - targetVelocity) < 60) {
+                    teamLED.setPosition(0.500);//green
+                }
+                else {
+                    teamLED.setPosition(0.283);//green
+                }
             }
         }
     }
@@ -140,7 +137,7 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio,
         // but only if at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double denominator = Math.max(abs(y) + abs(x) + abs(rx), 1);
         double frontLeftPower = (y + x + rx)*0.5 / denominator;
         double backLeftPower = (y - x + rx)*0.5 / denominator;
         double frontRightPower = (y - x - rx)*0.5 / denominator;
@@ -154,25 +151,40 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
 
     }
 
+
+    /**
+     * Function to calculate the heading and distance to the alliance goal.
+     */
+    private void goalHeading(){
+        //Instantiate the roadrunner Mecanum drive (via the OTOS localizer)
+        //SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, beginPose);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(-0, -0, Math.toRadians(0)));
+        drive.localizer.setPose(new Pose2d(-0, -0, Math.toRadians(0)));  //may have to do this for the new RR version per https://community.sparkfun.com/t/sparkfun-otos-with-ftc-inital-pose-always-0/67256
+
+        SparkFunOTOS.Pose2D pos = poseOTOS.getPosition(); //Read OTOS Pose for telemetry
+
+        if (gamepad1.right_trigger>0.1){
+            goalHeading = Math.atan2((goalY - pos.y),(goalX - pos.x));  //use ATAN2 function to calculate heading to goal corner
+            Actions.runBlocking(                                        //turn bot to heading using Roadrunner
+                    drive.actionBuilder(new Pose2d(pos.x,pos.y,pos.h))
+                            .turnTo(goalHeading)
+                    .build());
+        }
+    }
+
     /**
      * Manual control for the Core Hex powered feeder and the agitator servo in the hopper
      */
-    private void manualFeederAndagitatorControl() {
+    private void manualFeederControl() {
         // Manual control for the Core Hex agitator
         if (gamepad1.aWasPressed()) {
-            feeder.setPower(0.5);
+            feeder.setPower(-0.5);
         }
         else if (gamepad1.y) {
-            feeder.setPower(-0.5);
+            feeder.setPower(0.5);
         }
         else if (gamepad1.aWasReleased()) {
             feeder.setPower(0);
-        }
-        // Manual control for the hopper's servo
-        if (gamepad1.dpad_left) {
-            agitator.setPower(1);
-        } else if (gamepad1.dpad_right) {
-            agitator.setPower(-1);
         }
     }
 
@@ -185,20 +197,21 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
         if (gamepad1.options) {
             flywheel.setPower(-0.5);
         } else if (gamepad1.x) {
-            (flywheel).setPower(farVelocity);
-        } /*else if (gamepad1.right_bumper) {
-           flywheel.setPower(0.7);
-        } */   else if (gamepad1.b) {
-            flywheel.setPower(bankVelocity);
+            ((DcMotorEx) flywheel).setVelocity(farVelocity);
+            targetVelocity = farVelocity;
+        }else if (gamepad1.right_trigger >0.1){
+            bankShotAuto();
+        }else if (gamepad1.left_trigger >0.1){
+            farPowerAuto();
+        } else if (gamepad1.b) {
+            ((DcMotorEx) flywheel).setVelocity(bankVelocity);
+            targetVelocity = bankVelocity;
         } else if (gamepad1.left_bumper) {
-            flywheel.setPower(maxVelocity);
+            ((DcMotorEx) flywheel).setVelocity(maxVelocity);
+            targetVelocity = maxVelocity;
         } else {
             (flywheel).setPower(0);
             feeder.setPower(0);
-            // The check below is in place to prevent stuttering with the agitator. It checks if the agitator is under manual control!
-            if (!gamepad1.dpad_right && !gamepad1.dpad_left) {
-                agitator.setPower(0);
-            }
         }
     }
 
@@ -207,31 +220,27 @@ public class REVStarterBotTeleOpJava extends LinearOpMode {
      * When running this function, the flywheel will spin up and the Core Hex will wait before balls can be fed.
      * The agitator will spin until the bumper is released.
      */
-   /*  private void bankShotAuto() {
-        (flywheel).setPower(bankVelocity);
-        agitator.setPower(-1);
-       if (flywheel).getPower() >= bankVelocity - 50) {
+    private void farPowerAuto() {
+        ((DcMotorEx) flywheel).setVelocity(farVelocity);
+        if (((DcMotorEx) flywheel).getVelocity() >= farVelocity - 100) {
             feeder.setPower(1);
         } else {
             feeder.setPower(0);
         }
-    }*/
+    }private void bankShotAuto() {
+        (flywheel).setPower(bankVelocity);
+       if (((DcMotorEx) flywheel).getVelocity()>= bankVelocity - 50) {
+            feeder.setPower(1);
+        } else {
+            feeder.setPower(0);
+        }
+    }
 
     /**
      * The far power velocity is intended for launching balls a few feet from the goal. It may require adjusting the deflector.
      * When running this function, the flywheel will spin up and the Core Hex will wait before balls can be fed.
      * The agitator will spin until the bumper is released.
      */
-   /* private void farPowerAuto() {
-        (flywheel).setPower(farVelocity);
-        int farVelocity1 = (farVelocity);
-        agitator.setPower(-1);
-        if (flywheel).getVelocity() >= farVelocity1 - 100) {
-            feeder.setPower(1);
-        } else {
-            feeder.setPower(0);
-        }
-    }*/
 
     private void configureOtos() {
         telemetry.addLine("Configuring OTOS...");
