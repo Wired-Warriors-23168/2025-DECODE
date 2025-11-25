7190d93e022f9fbbba9c5c0caa1539e75d7a237f
a-krier <kriergenics@gmail.com>
2025-11-25 18:56:30 -0500

    Update AUTO_Test_Pathing_Brenden.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO_Test_Pathing_Brenden.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO_Test_Pathing_Brenden.java
@@ -23,7 +23,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @Config //Required to be able to tune parameters in FTCDashboard
-@Autonomous(name="AUTO_Test_Pathing_Brenden", group="AUTO", preselectTeleOp = "TELEOP_MAIN")
+@Autonomous(name="AUTO_Test_Pathing_Brendan", group="AUTO", preselectTeleOp = "TELEOP_MAIN")
 public class AUTO_Test_Pathing_Brenden extends LinearOpMode {
     /////////////////////////////////////////////////////////////////////////
     // Declare variables
